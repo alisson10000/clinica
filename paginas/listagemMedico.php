0fdb2176f7be3dcc34bfc8e59e7ba28e3e293491
alisson10000 <alisson_lima_souza@yahoo.com.br>
2024-05-21 19:16:21 -0300

    Update listagem medico

--- paginas/listagemMedico.php
+++ paginas/listagemMedico.php
@@ -1,1 +1,32 @@
-<h1>listagem registro médico</h1>
+<?php
+include './servicos/conexao.php';
+
+$query = "Select * from medicos";
+
+$query = mysqli_query($conexao, $query);
+?>
+<table border="1">
+    <tr>
+        <td>matricula</td>
+        <td>nome</td>
+        <td>crm</td>
+        <td>nascimento</td>
+    </tr>
+    <?php
+    while ($row = mysqli_fetch_array($query)) {
+        ?>
+        <tr>
+            <td><?php echo $row['idMedico']; ?></td>
+            <td><?php echo utf8_encode($row['nomeMedico']); ?></td>
+            <td><?php echo $row['crmMedico']; ?></td>
+            <td><?php echo $row['nascimentoMedico']; ?></td>
+        </tr>
+
+        <?php
+    }
+    ?>
+</table>
+
+
+
+
