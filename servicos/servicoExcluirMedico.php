7499111841de7ebf46997d032cf3d621b5aa1988
alisson10000 <alisson_lima_souza@yahoo.com.br>
2024-05-21 20:29:54 -0300

    exclusão

---
+++ servicos/servicoExcluirMedico.php
@@ -0,0 +1,38 @@
+<?php
+
+require './conexao.php';
+$idMedico = filter_input(INPUT_POST, 'idMedico', FILTER_DEFAULT);
+
+$query = "Select * from medicos where idMedico=$idMedico";
+
+$query = mysqli_query($conexao, $query);
+
+$contagem = mysqli_num_rows($query);
+
+if ($contagem > 0) {
+    $query = "DELETE FROM medicos WHERE idMedico=$idMedico";
+    mysqli_query($conexao, $query);
+
+    $query = "Select * from medicos where idMedico=$idMedico";
+
+    $query = mysqli_query($conexao, $query);
+
+    $contagem = mysqli_num_rows($query);
+
+    if ($contagem == 0) {
+        header("Location: http://localhost/clinica/index.php?pagina=4&mensagem=1");
+    } else if ($contagem > 0) {
+        header("Location: http://localhost/clinica/index.php?pagina=4&mensagem=2");
+    }
+} else {
+    header("Location: http://localhost/clinica/index.php?pagina=4&mensagem=3");
+}
+ 
+ 
+ 
+
+
+
+
+
+
