80f958841ab8ce95ed57b26390d0e9f8a124e590
alisson10000 <alisson_lima_souza@yahoo.com.br>
2024-06-04 21:28:55 -0300

    update
    criação de session

--- index.php
+++ index.php
@@ -1,25 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
-  <link rel="stylesheet" href="./css/raiz.css">
-</head>
-<body>
-  <div class="login-container">
-    <h2>Login</h2>
-    <form action="index.php" method="post">
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username" required>
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" required>
-      </div>
-      <button type="submit">Login</button>
-    </form>
-  </div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+        <link rel="stylesheet" href="../css/raiz.css">
+        <style>@import "css/raiz.css";</style>
+
+    </head>
+    <body>
+        <div class="login-container">
+            <h2>Login</h2>
+            <form action="servicos/logar.php" method="post">
+                <div class="form-group">
+
+                    <select name="role">
+                        <option>-- selecione o tipo de usuario -- </option>
+                        <option value="ADMINISTRADOR">ADMINISTRADOR</option>
+                        <option value="BASICO">BÁSICO</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            <?php
+            if (isset($_GET['mensagem'])) {
+
+
+                $mensagem = $_GET['mensagem'];
+
+                if ($mensagem == 0) {
+                    ?>
+
+                    <div class="erro">
+                        login ou senha inválida!
+                    </div>
+
+
+                    <?php
+                } elseif ($mensagem == 1) {
+                    ?>
+
+                    <div class="erro">
+                        Usuario não autenticado no sistema!
+                    </div>
+
+
+                    <?php
+                }
+                elseif ($mensagem == 2) {
+                    ?>
+
+                    <div class="erro">
+                        Você selecionou o tipo errado!!!
+                    </div>
+
+
+                    <?php
+                }
+            }
+            ?>
+
+        </div>
+    </body>
 </html>
+
