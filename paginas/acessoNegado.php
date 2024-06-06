8a10efa84c63548d9b9d825575de962dc77765cb
alisson10000 <alisson_lima_souza@yahoo.com.br>
2024-06-06 18:44:41 -0300

    delete
    delet

--- paginas/acessoNegado.php
+++
@@ -1,6 +0,0 @@
- <div class="acesso-negado-container">
-        <h1>Acesso Negado</h1>
-        <p>Você não tem permissão para acessar esta página.</p>
-        <img src="./img/acesso_negado.jfif" alt="Acesso Negado">
-        <a href="home.php" class="botao-voltar">Voltar para a página inicial</a>
-    </div>
